cf1350d621938bc8f996339566f500969e4b41a1
lsankar4033 <lsankar4033@gmail.com>
2022-07-12 16:11:21 -0400

    Dummy proof file

---
+++ src/groth16prove.rs
@@ -0,0 +1,27 @@
+use ark_circom::{CircomBuilder, CircomConfig};
+use ark_std::rand::thread_rng;
+
+use ark_bn254::Bn254;
+use ark_groth16::{
+    create_random_proof as prove, generate_random_parameters, prepare_verifying_key, verify_proof,
+};
+
+use wasm_bindgen::prelude::*;
+
+// TODO: inputs are filenames for wasm/r1cs, witness bigints
+// TODO: JsValue is a wrapper around a rust value, so we can use it to pass in the witness
+#[wasm_bindgen]
+pub fn prove(wasm_file: String, r1cs_file: String) {
+    let cfg = CircomConfig::<Bn254>::new(wasm_file, r1cs_file)?;
+    let mut builder = CircomBuilder::new(cfg);
+
+    let circom = builder.setup();
+    // builder.set_witness(witness);
+
+    let mut rng = thread_rng();
+    let params = generate_random_parameters::<Bn254, _, _>(circom, &mut rng)?;
+
+    let circom = builder.build()?;
+
+    let proof = prove(circom, &params, &mut rng)?;
+}
